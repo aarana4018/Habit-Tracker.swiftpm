a3be964768699a2f236992b71fdb9fa49fbde87b
Andrea Arana <aarana4018@stu.d214.org>
2023-05-02 13:31:41 -0500

    Started the beginning of the Water Tracker

--- WaterTracker.swift
+++ WaterTracker.swift
@@ -2,9 +2,37 @@ import SwiftUI
 
 struct WaterView: View {
     
+    @State var WaterGoal: Double? = 0.0 
+    @State var WaterIntake = 0.0
+    @State var WaterFill = 0.0
+    @State var RestatedWaterGoal = "Your Goal Here"
+    
+    @State var CloudOne = "cloud"
+    @State var CloudTwo = "cloud"
+    @State var CloudThree = "cloud"
+    @State var CloudFour = "cloud"
+    
+    
     var body: some View {
         
-        Text("")
+        VStack {
+            
+//            Slider(value: $WaterIntake, in: 0...(WaterGoal ?? 72.0), step: 1)
+            
+            HStack {
+                
+                TextField("Your Goal", value: $WaterGoal, format: .number)
+                    .textFieldStyle(.roundedBorder)
+                    .onSubmit {
+                        RestatedWaterGoal = "\(WaterGoal ?? 72)"
+                    }
+                
+                Text(RestatedWaterGoal)
+            }
+            .padding()
+            
+        }
+        .padding()
         
     }
 }
